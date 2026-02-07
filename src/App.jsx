028d0f9a510316208390a3a46157c7938c1f0019
alex-zander-0403 <alexzander0403@gmail.com>
2026-02-07 21:26:47 +0300

    add env enviroment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <h1>hello, ci/cd!</h1>;
+  return (
+    <div>
+      <h1>hello, ci/cd!</h1>
+      <h2>with, {import.meta.env.VITE_API_KEY}</h2>
+    </div>
+  );
 }
 
 export default App;
