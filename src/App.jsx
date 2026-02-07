1e28c60666a4e883e434d0873e36354620f05488
alex-zander-0403 <alexzander0403@gmail.com>
2026-02-07 14:36:58 +0300

    ready for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <h1>hello, git!</h1>;
+  return <h1>hello, ci/cd!</h1>;
 }
 
 export default App;
